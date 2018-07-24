4a37ebea7892debc1d6be52ed068385993adb521
Agustín González F <awugonzalez@gmail.com>
2018-07-24 19:37:56 -0400

    comments firebase.ts

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,31 +1,45 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
 //Firebase
 import { AngularFireAuth } from "angularfire2/auth";
 import { AngularFirestore } from "angularfire2/firestore";
-/*
-  Generated class for the FirebaseProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+
 @Injectable()
 export class FirebaseProvider {
+
+  /**
+  * Reference Object used for Subscriptions.
+  **/
+  private ref;
 
   constructor(
     private afAuth: AngularFireAuth,
     private af: AngularFirestore
   ) {}
 
+  /**
+  * isAthenticated
+  * @description : Return TRUE/FALSE if the User is authenticated.
+  * @returns : boolean
+  */
   public isAuthenticated(): boolean {
     return this.afAuth.auth.currentUser.uid != null;
-  }
+  } //isAuthenticated
 
-  public async login(email:string, password: string): Promise<any> {
+  /**
+  * login
+  * @description : 
+  * @returns : Promise<any>
+  **/
+  public login(email:string, password: string): Promise<any> {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password);
-  }
+  } //login
+
+  public logout() : Promise<void> {
+    //return this.afAuth.auth
+  } //logout
 
   
-}
+} //FirebaseProvider
